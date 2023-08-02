7c259d0e627e73cff345965c906675e823efbe5e
Michael Napitupulu <asm34686@gmail.com>
2023-08-02 08:42:07 +0700

    user-controller JwtGuard

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,18 +1,21 @@
-import { Controller, Param, Get } from '@nestjs/common';
+import { Controller, Param, Get, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserDetails } from './user-details.interface';
 import { UserDocument } from './user.schema';
+import { JwtGuard } from 'src/auth/guard/jwt.guard';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Get(':id')
+  @UseGuards(JwtGuard)
   async getUser(@Param('id') id: string): Promise<UserDetails | null> {
     return this.userService.findById(id);
   }
 
   @Get()
+  @UseGuards(JwtGuard)
   async listAllUser(): Promise<UserDocument[]> {
     return this.userService.getAll();
   }
