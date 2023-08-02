b514ca2fc027592fb65d9a0600263f7d14ba4653
Michael Napitupulu <asm34686@gmail.com>
2023-08-02 08:38:22 +0700

    add AuthGuard with JwtGuard

--- backend/src/product/product.controller.ts
+++ backend/src/product/product.controller.ts
@@ -8,15 +8,18 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductDocument } from './product.schema';
+import { JwtGuard } from 'src/auth/guard/jwt.guard';
 
 @Controller('product')
 export class ProductController {
   constructor(private productService: ProductService) {}
 
   @Post()
+  @UseGuards(JwtGuard)
   async createPost(
     @Body('name') name: string,
     @Body('price') price: number,
@@ -26,16 +29,19 @@ export class ProductController {
   }
 
   @Get()
+  @UseGuards(JwtGuard)
   async findAll(): Promise<ProductDocument[]> {
     return await this.productService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(JwtGuard)
   async findProduct(@Param('id') id: string): Promise<ProductDocument> {
     return await this.productService.findById(id);
   }
 
   @Patch(':id')
+  @UseGuards(JwtGuard)
   async updateProduct(
     @Param('id') id: string,
     @Body('name') name: string,
@@ -53,6 +59,7 @@ export class ProductController {
   }
 
   @Delete(':id')
+  @UseGuards(JwtGuard)
   async removeProduct(@Param('id') id: string) {
     const existingProduct = await this.productService.findById(id);
     if (!existingProduct) {
