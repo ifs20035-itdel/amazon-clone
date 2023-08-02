056019a76690a40385826c7347ad5bf763d105a5
Michael Napitupulu <asm34686@gmail.com>
2023-08-02 12:32:57 +0700

    add react_project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {ThemeProvider} from '@mui/material';
+
+import { theme } from './shared/utils/theme';
+
+import HomePage from './pages/Home.page';
+import RegisterPage from './pages/Register.page';
+import SignInPage from './pages/SignIn.page';
+
+function App() {
+  return <ThemeProvider theme={theme}>
+    <Router>
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/register' element={<RegisterPage/>} />
+        <Route path='/signin' element={<SignInPage/>} />
+      </Routes>
+    </Router>
+  </ThemeProvider>;
+}
+
+export default App;
