9acd706d4f838b6b69531820770b607b84284f4e
ifs20035-itdel <ifs20035@students.del.ac.id>
2023-08-09 23:24:01 +0700

    add handler

--- frontend/src/hooks/use-input.ts
+++ frontend/src/hooks/use-input.ts
@@ -1,3 +1,4 @@
+import { ChangeEvent, useReducer } from "react";
 import { Action } from "../shared/models/action.interface";
 import { INPUT_ACTION_BLUR, INPUT_ACTION_CHANGE, INPUT_ACTION_CLEAR, InputActionType } from "./models/inputAction";
 import { InputState } from "./models/inputStateInterface";
@@ -21,3 +22,32 @@ const inputReducer = (state: InputState, action: Action<InputActionType>) => {
       return {...state};
   }
 }
+
+const useInput = () => {
+  const [{ text, hasBeenTouched }, dispatch] = useReducer(
+    inputReducer, 
+    initialInputState,
+  );
+
+  const textChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({ type: INPUT_ACTION_CHANGE, value: e.target.value })
+  }
+
+  const inputBlurHandler = () => {
+    dispatch({ type: INPUT_ACTION_BLUR })
+  }
+
+  const clearHandler = () => {
+    dispatch({ type: INPUT_ACTION_CLEAR })
+  }
+
+  return {
+    text,
+
+    textChangeHandler,
+    inputBlurHandler,
+    clearHandler,
+  }
+};
+
+export default useInput;
