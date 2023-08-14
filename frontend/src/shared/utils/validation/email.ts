341fb98dc0f962ecd810644a88c4a88bc6706c7a
ifs20035-itdel <ifs20035@students.del.ac.id>
2023-08-14 21:07:30 +0700

    start-week

--- frontend/src/shared/utils/validation/email.ts
+++ frontend/src/shared/utils/validation/email.ts
@@ -2,5 +2,5 @@ import { validatorFn } from "./models/ValidatorFn";
 
 export const ValidateEmail: validatorFn = (text: string): boolean => {
     const re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
-    return re.test(email.trim());
+    return re.test(text.trim());
 }
