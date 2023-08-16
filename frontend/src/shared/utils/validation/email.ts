44216a6f7675ad276df5e6894f3c82ac880670b8
ifs20035-itdel <ifs20035@students.del.ac.id>
2023-08-16 22:45:58 +0700

    add interface for registerfield

--- frontend/src/shared/utils/validation/email.ts
+++ frontend/src/shared/utils/validation/email.ts
@@ -1,6 +1,7 @@
 import { validatorFn } from "./models/ValidatorFn";
 
 export const ValidateEmail: validatorFn = (text: string): boolean => {
+    //RFC2822
     const re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
     return re.test(text.trim());
 }
