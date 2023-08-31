ec651511630ba08fe9b40d7460695444eff7795f
ifs20035-itdel <ifs20035@students.del.ac.id>
2023-08-31 16:44:03 +0700

    update

--- frontend/src/features/auth/authSlice.ts
+++ frontend/src/features/auth/authSlice.ts
@@ -1,4 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { DisplayUser } from "./models/DisplayUser.interface";
+import { Jwt } from "./models/Jwt";
 
 interface AsyncState{
   isLoading: boolean;
@@ -6,11 +8,25 @@ interface AsyncState{
   isError: boolean;
 }
 
-interface AuthState extends AsyncState
+interface AuthState extends AsyncState {
+  user?: DisplayUser | null;
+  jwt?: Jwt;
+  isAuthenticated?: boolean;
+}
 
-const initialState: 
+const initialState: AuthState = {
+  user: null, // user,
+  jwt: null, // jwt,
+  isAuthenticated?: false,
+  isLoading: false,
+  isSuccess: false,
+  isError: false,
+}
 
 export const authSlice = createSlice({
   name: 'auth',
-  initialState: 
+  initialState,
+  reducers: {
+    
+  }
 });
